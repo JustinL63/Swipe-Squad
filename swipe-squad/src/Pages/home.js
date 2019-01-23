2df1332109dec8a316475cf37407d8ae4e9a378d
Justin Lamb <lambjg63@gmail.com>
2019-01-23 14:19:22 -0500

    third times a charm

---
+++ swipe-squad/src/Pages/home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Proflie from "..components/Proflie";
+
+
+function Home () {
+    return(
+        <div>
+            <Profile>
+            <h2> This is where your Profile will appear</h2>
+
+            </Profile>
+
+        <h1>Welcome to Swipe Squad!</h1>
+
+
+        </div>
+
+    )
+}
+
+export default Home;
