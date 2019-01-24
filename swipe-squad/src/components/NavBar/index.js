b2bf62d6f8936ca51930a57b4ec1399fe5e0e127
Danielle Wood <danielle.wood9988@gmail.com>
2019-01-24 10:59:20 -0500

    footer and nav fixes

--- swipe-squad/src/components/NavBar/index.js
+++ swipe-squad/src/components/NavBar/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
+const navStyle = {
+  textAlign: 'center',
+}
+
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
